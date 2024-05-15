5f1d11c0ec72014b38c842c55946409c18897168
maperez1225 <martinperez_lopez@hotmail.com>
2024-05-15 13:55:27 -0500

    Added DB charts

--- web/vue.config.js
+++ web/vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     progress: false,
     proxy: {
       '^/api': {
-        target: 'http://api',
+        target: 'http://api:80',
         changeOrigin: true,
         secure: false,
         logLevel: 'debug',
